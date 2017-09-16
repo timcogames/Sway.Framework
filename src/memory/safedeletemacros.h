47eed87b3e8bbf9a2beb0313f645765f2f2d3d92
Victor Timoshin <victor-timoshin@hotmail.com>
2017-09-16 15:23:22 +0300

    Добавлены макросы безопасного удаления

---
+++ src/memory/safedeletemacros.h
@@ -0,0 +1,22 @@
+#ifndef SWAY_MEMORY_SAFEDELETEMACROS_H
+#define SWAY_MEMORY_SAFEDELETEMACROS_H
+
+/*!
+ * \brief Безопасно удаляет объект и приводит указатель к NULL.
+ *
+ * \param object Объект для удаления.
+ * \see SAFE_DELETE_ARRAY
+ */
+#undef  SAFE_DELETE
+#define SAFE_DELETE(object) if (object != NULL) { delete object; object = NULL; }
+ 
+/*!
+ * \brief Безопасно удаляет массив и приводит указатель к NULL.
+ *
+ * \param array Массив для удаления.
+ * \see SAFE_DELETE
+ */
+#undef  SAFE_DELETE_ARRAY
+#define SAFE_DELETE_ARRAY(array) if (array != NULL) { delete[] array; array = NULL; }
+
+#endif // SWAY_MEMORY_SAFEDELETEMACROS_H
