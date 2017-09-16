5b469106e1c5e46ed4dfecf55ede66f8c1d785c1
Victor Timoshin <victor-timoshin@hotmail.com>
2017-09-16 14:31:32 +0300

    Добавлен модульный тест.

---
+++ src/tests/foundation.test.cpp
@@ -0,0 +1,45 @@
+#define BOOST_TEST_MODULE SubsystemFixture
+#include <boost/test/included/unit_test.hpp>
+
+#include "../defines.h"
+#include "../memory/safedeletemacros.h"
+#include "../foundation/objectmetadata.h"
+#include "../foundation/object.h"
+#include "../foundation/declareobjectmacros.h"
+#include "../foundation/context.h"
+
+USING_NAMESPACE(sway)
+
+class ThingyContext : public foundation::Context
+{
+ public:
+	ThingyContext() {}
+	virtual ~ThingyContext() {}
+};
+
+class ThingyObject : public foundation::Object
+{
+	DECLARE_OBJECT(ThingyObject, foundation::Object)
+
+ public:
+	ThingyObject(foundation::Context *context): foundation::Object(context) {}
+	~ThingyObject() {}
+};
+
+BOOST_AUTO_TEST_SUITE(test_suite)
+BOOST_AUTO_TEST_CASE(test_case)
+{
+	ThingyContext *context = new ThingyContext();
+	context->registerObject(new ThingyObject(context));
+
+	ThingyObject *object = (ThingyObject *)context->getObject("ThingyObject");
+	BOOST_CHECK(object->getClassName() == "ThingyObject");
+	BOOST_CHECK(context->getObjectCount() == 1);
+
+	context->unregisterObject("ThingyObject");
+	BOOST_CHECK(context->getObjectCount() == 0);
+
+	SAFE_DELETE(object);
+	SAFE_DELETE(context);
+}
+BOOST_AUTO_TEST_SUITE_END()
