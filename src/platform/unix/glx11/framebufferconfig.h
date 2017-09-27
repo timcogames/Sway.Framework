94fccae2add8268051b0d64c3f3fb979ac8f45d2
Victor Timoshin <victor-timoshin@hotmail.com>
2017-09-28 02:16:28 +0300

    Добавлена структура FrameBufferConfig

---
+++ src/platform/unix/glx11/framebufferconfig.h
@@ -0,0 +1,33 @@
+#ifndef SWAY_PLATFORM_UNIX_GLX11_FRAMEBUFFERCONFIG_H
+#define SWAY_PLATFORM_UNIX_GLX11_FRAMEBUFFERCONFIG_H
+
+#include "glx11prereqs.h"
+
+NAMESPACE_BEGIN(sway)
+NAMESPACE_BEGIN(glx11)
+
+struct FrameBufferConfig
+{
+	s32 configId;
+	s32 visualId;
+	s32 visualType;
+	s32 bufferSize;
+	s32 level;
+	s32 renderType;
+	s32 doubleBuffer;
+	s32 stereo;
+	s32 auxBuffers;
+	s32 redSize;
+	s32 greenSize;
+	s32 blueSize;
+	s32 alphaSize;
+	s32 depthSize;
+	s32 stencilSize;
+	s32 numMultisample;
+	s32 numSamples;
+};
+
+NAMESPACE_END(glx11)
+NAMESPACE_END(sway)
+
+#endif // SWAY_PLATFORM_UNIX_GLX11_FRAMEBUFFERCONFIG_H
